23e872be905747c15fc2961b709ba821fb34fcc7
Arion <arionnagappen@gmail.com>
2026-03-07 16:17:04 +0200

    setup & initialized terraform directory

---
+++ terraform/backend.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket = "mycloudrecovery-tfstate"
+    key = "terraform/terraform.tfstate"
+    region = "eu-west-1"
+    use_lockfile = true
+    encrypt = true
+  }
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 6.0"
+   }
+
+   archive = {
+    source = "hashicorp/archive"
+    version = "~> 2.0"
+   }
+  }
+}
